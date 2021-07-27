b15ea9f0dc468fcd9df96a0e17083973269703c5
Carlos Martinez <curlosmart@gmail.com>
2021-07-27 13:35:14 -0400

    convert temperatures successfully with button

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
+const degreesElem = document.getElementById("degrees")
+const tempTypeElem = document.getElementById("temperature-type")
+const convertButton = document.getElementById("convertButton")
+
 const conversions = {
-    'farenheit': function convert(tempFarenheit) {
+    'fahrenheit': function convert(tempFahrenheit) {
         return {
-            'celsius': ((tempFarenheit - 32) * 5/9),
-            'kelvin': ((tempFarenheit - 32) * 5/9 + 273.15)
+            'celsius': ((tempFahrenheit - 32) * 5/9),
+            'kelvin': ((tempFahrenheit - 32) * 5/9 + 273.15)
         }
     },
     'celsius': function convert(tempCelsius) {
         return {
-           'farenheit': ((tempCelsius * 9/5) + 32), 
+           'fahrenheit': ((tempCelsius * 9/5) + 32), 
            'kelvin': (tempCelsius + 273.15)
         }
     },
     'kelvin': function convert(tempKelvin) {
         return {
-            'farenheit': ((tempKelvin - 273.15) * 9/5 + 32), 
+            'fahrenheit': ((tempKelvin - 273.15) * 9/5 + 32), 
             'celsius': (tempKelvin - 273.15)
         }
     }
 }
 
-console.log(Number(conversions.farenheit(72).celsius.toFixed(4)))
+const tempSymbols = {
+    'fahrenheit': '°F',
+    'celsius': '°C',
+    'kelvin': '°K'
+}
+
+const convertTemperature = () => {
+    const degrees = degreesElem.value
+    const tempType = tempTypeElem.value
+
+    console.log(conversions[tempType](degrees))
+}
+console.log(Number(conversions.fahrenheit(72).celsius.toFixed(4)))
+
+convertButton.addEventListener('click', convertTemperature)
